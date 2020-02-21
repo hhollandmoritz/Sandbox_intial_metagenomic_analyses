12b0e5ef8a5429b4d0b4248ac5918ec7dbda687a
hollandh <hhollandmoritz@gmail.com>
2020-02-20 19:44:47 -0700

    write script for running initial fastqc

--- scripts/01_run_fastqc.sh
+++ scripts/01_run_fastqc.sh
@@ -4,23 +4,29 @@
 # create_cutadapt_commands.sh
 
 # Define variables; FILL THIS PART IN
-PROJ_DIR=/scratch/summit/$USER/austrailian-metagenome/first_50_test_samples
-STAGE_DIR=$PROJ_DIR/01_preprocess
-COMMAND_DIR=$PROJ_DIR/01_preprocess/01_fastqc_commands # location to place the command files
+PROJ_DIR=/data/hollandh/CitSciSandbox/Metagenomic_analyses
+OUTPUTS=$PROJ_DIR/outputs
+LOGS=$PROJ_DIR/logs
+STAGE=01_preprocess
+STAGE_OUT_DIR=$OUTPUTS/$STAGE
+STAGE_LOG_DIR=$LOGS/$STAGE
+SCRIPT_DIR=$PROJ_DIR/scripts/01_run_fastqc.sh # location of script file
 IN_DIR=$PROJ_DIR/00_raw_data # location of raw fastq files
-OUT_DIR=$STAGE_DIR/00_qual_check # location of output quality assessment files
-CORES=1 # number of cores for fastqc to use
-
+OUT_DIR=$STAGE_OUT_DIR/00_qual_check # location of output quality assessment files
+LOG_DIR=$STAGE_LOG_DIR/00_qual_check # Location of logs from quality assessment
+# Parallel variables
+# Note: on proteus CORES*ntasks should not exceed 32
+CORES=8 # number of cores for fastqc to use
+ntasks=2
 
 # Move into datafile
 cd $IN_DIR
 
-# make an file to put the command files in
-mkdir -p $COMMAND_DIR
-
-# make a directory to put the output in
+# make a directories to put outputs and logs
 mkdir -p $OUT_DIR
+mkdir -p $LOG_DIR
 
+j=0
 # make commands and write them to a file
 for i in $(ls *.fastq.gz)
 do
@@ -31,29 +37,26 @@ do
     infile="$IN_DIR"/"$prefix".fastq.gz
     echo $infile
     outfile="$prefix".fastq.gz
-    command_file_name=$COMMAND_DIR/"$prefix"_fastqc_command.sbatch
-    logfile_name=$COMMAND_DIR/"$prefix"_fastqc.log
+    logfile_name=$LOG_DIR/"$prefix"_fastqc.log
     
     ## Writing the batch script (note: formatting without indentation here is imporant)
 
-cat << EOF > $command_file_name    
-#!/bin/bash
+    # cd into the output directory
+    cd $IN_DIR 
+    mkdir -p $OUT_DIR # Make output directory before running
 
-# cd into the output directory
-cd $IN_DIR 
-mkdir -p $OUT_DIR # Make output directory before running
+    # Run Fastqc command
+    #fastqc $infile -o $OUT_DIR -t $CORES --extract > logfile_name
 
-# Run Fastqc command
-fastqc $infile -o $OUT_DIR -t 1 --extract #for zipped files use the --extract flag
+    # create non-leading zero version
+    jeval=$(expr $j + 0)
+    if (($jeval % $ntasks == 0))
+        then
+        wait
+        echo j is "$j". Waiting for processes to finish before continuing...
+    fi
 
-# write an empty .end file into the command directory so user can keep track while program runs
-touch $command_file_name.end
-    
-EOF
 
 done
 
-# Make a file that lists the location of the commands
 
-cd $COMMAND_DIR
-ls $PWD/*.sbatch > commands.txt

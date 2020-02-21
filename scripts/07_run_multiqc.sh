425b32913b33503d0e8b8eb83585ed4c411e41fa
hollandh <hhollandmoritz@gmail.com>
2020-02-20 21:33:55 -0700

    creates multiqc analysis script for end of raw data cleaning

---
+++ scripts/07_run_multiqc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Set up environment:
+# Load Conda Environment
+source $(conda info --base)/etc/profile.d/conda.sh
+conda activate multiqc
+
+# Set up directories
+PROJ_DIR=/data/hollandh/CitSciSandbox/Metagenomic_analyses
+STAGE_DIR=$PROJ_DIR/01_preprocess
+MULT_QC_DIR=$STAGE_DIR/multiqc_output
+
+cd $STAGE_DIR
+
+mkdir -p $MULT_QC_DIR
+
+multiqc 00_qual_check 01_cutadapt_transposase_commands 01_qual_check 02_qual_check -o $MULT_QC_DIR -c 05_multiqc_config.yaml -n 03_multiqc_report_post_cleaning.html --interactive -f
